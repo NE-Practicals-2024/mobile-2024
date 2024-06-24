4487dbf714d8c8e7cfcac10f21ef41689c309adf
Precieux Mugisha <precieuxmugisha@gmail.com>
2024-06-24 12:47:06 +0200

    [feat]: Add view comments

--- mobile/app/post/[id].tsx
+++ mobile/app/post/[id].tsx
@@ -3,7 +3,7 @@ import { IComment, IPost } from '@/types';
 import { AntDesign } from '@expo/vector-icons';
 import { Stack, router, useLocalSearchParams } from 'expo-router';
 import React, { useEffect, useState } from 'react';
-import { Image, SafeAreaView, Text, TouchableOpacity } from 'react-native';
+import { Image, SafeAreaView, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import { useToast } from 'react-native-toast-notifications';
 
 export default function PostScreen() {
@@ -29,17 +29,29 @@ export default function PostScreen() {
 
     return (
         <SafeAreaView className='w-full flex-1 flex-col flex pt-24 px-3'>
-            <Stack.Screen options={{ headerShown: false }} />
-            <TouchableOpacity onPress={() => router.back()} className='absolute top-14 left-4 bg-primary rounded-full w-10 h-10 flex items-center justify-center'>
-                <AntDesign name="arrowleft" size={24} color="white" />
-            </TouchableOpacity>
-            <TouchableOpacity onPress={removePost} className='absolute top-14 right-4 bg-red-600 rounded-full w-10 h-10 flex items-center justify-center'>
-                <AntDesign name="delete" size={24} color="white" />
-            </TouchableOpacity>
-            <Text className='text-lg my-4 font-semibold'>{post?.title}</Text>
-            <Text className='mb-4 italic text-slate-700'>Written By: John Doe</Text>
-            <Image className='w-full h-44 rounded-lg' source={{ uri: "https://picsum.photos/500/500" }} />
-            <Text className='my-4 text-[15px]'>{post?.body}</Text>
+            <ScrollView>
+                <Stack.Screen options={{ headerShown: false }} />
+                <View className='w-full flex flex-row justify-between items-center'>
+                <TouchableOpacity onPress={() => router.back()} className='ml-4 bg-primary rounded-full w-10 h-10 flex items-center justify-center'>
+                    <AntDesign name="arrowleft" size={24} color="white" />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={removePost} className='mr-4 bg-red-600 rounded-full w-10 h-10 flex items-center justify-center'>
+                    <AntDesign name="delete" size={24} color="white" />
+                </TouchableOpacity>
+                </View>
+                <Text className='text-lg my-4 font-semibold'>{post?.title}</Text>
+                <Text className='mb-4 italic text-slate-700'>Written By: John Doe</Text>
+                <Image className='w-full h-44 rounded-lg' source={{ uri: "https://picsum.photos/500/500" }} />
+                <Text className='my-4 text-[15px]'>{post?.body}</Text>
+                <Text className='my-3 font-semibold'>Comments</Text>
+                <View className='w-full'>
+                    {comments.map((comment, index) => (
+                        <View key={index} className='w-full bg-gray-300 p-3 my-2 rounded-lg'>
+                            <Text className='text-sm'>{comment.body}</Text>
+                        </View>
+                    ))}
+                </View>
+            </ScrollView>
         </SafeAreaView>
     );
 }
